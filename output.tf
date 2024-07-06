f682365d384c9039c299d80be5a4228a31430bbe
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-07-06 13:30:25 +0530

    output

--- output.tf
+++ output.tf
@@ -7,3 +7,24 @@
 output "vpc_id" {
   value = aws_vpc.main.id
 }
+
+
+output "public_subnet_ids" {
+  value = aws.public_subnet_ids[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws.private_subnet_ids[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws.database_subnet_ids[*].id
+}
+
+output "database_subnet_group_id" {
+  value = aws_db_subnet_group.default.id
+}
+
+output "igw_id" {
+  value = aws_internet_gateway.gw.id
+}
